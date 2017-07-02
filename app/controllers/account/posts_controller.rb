2a9394a7475d1afdc453f9b4203d9195ff2cda57
cherryan <yancherryyan@gmail.com>
2017-07-02 21:36:17 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,15 @@ class Account::PostsController < ApplicationController
  def index
    @posts = current_user.posts
  end
+
+ def edit
+   @post = Group.find(params[:id])
+end
+
+ def destroy
+     @Group = Group.find(params[:id])
+     @post.destroy
+
+     redirect_to posts_path
+   end
 end
